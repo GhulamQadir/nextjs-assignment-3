3f758b510d00ca4fc553962062a4fe94fc895840
Ghulam Qadir <hafizghulamqadir.2005@gmail.com>
2024-11-12 16:47:40 +0500

    updated changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
 import Navbar from "./Components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      {/* <p className="text-6xl font-bold">Assignment 3</p> */}
       <Navbar />
     </div>
   );
