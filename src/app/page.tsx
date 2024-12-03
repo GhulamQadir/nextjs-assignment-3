855ddee66b3cf1b53f2ba963f5e48f8f101ede84
Ghulam Qadir <hafizghulamqadir.2005@gmail.com>
2024-12-03 15:01:39 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import WorkManagement from "@/components/Work-Management";
+import WorkManagement from "../components/Work-Management";
 import Banner from "../components/Banner";
 import Navbar from "../components/Navbar";
-import ExtensionBanner from "@/components/ExtensionBanner";
-import CustomizeNeeds from "@/components/CustomizeNeeds";
-import WorkEveryWhere from "@/components/WorkEveryWhere";
-import Sponsors from "@/components/Sponsors";
-import Footer from "@/components/Footer";
+import ExtensionBanner from "../components/ExtensionBanner";
+import CustomizeNeeds from "../components/CustomizeNeeds";
+import WorkEveryWhere from "../components/WorkEveryWhere";
+import Sponsors from "../components/Sponsors";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
